9f1176d96f3b0c6771d6b2b9df680b51faab5ecd
Lucien NTUMBA <admin@theartnewspaper.com>
2017-02-03 15:40:27 +0000

    fix plugin installation problem

--- roles/wordpress/files/wp-config.php
+++ roles/wordpress/files/wp-config.php
@@ -37,9 +37,6 @@ define('DB_CHARSET', 'utf8');
 /** The Database Collate type. Don't change this if in doubt. */
 define('DB_COLLATE', '');
 
-/** Install/Upgrade WordPress plugins without providing FTP access. */
-define('FS_METHOD', 'direct');
-
 /**#@+
  * Authentication Unique Keys and Salts.
  *
